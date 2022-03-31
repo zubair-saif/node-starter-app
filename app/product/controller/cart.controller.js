748dbc9dbd47ac46c4e7482d3243df6c7b58f6d4
zubair-saif <zubairsaif700@gmail.com>
2022-03-31 22:17:00 +0500

    cart api fixed

--- app/product/controller/cart.controller.js
+++ app/product/controller/cart.controller.js
@@ -1,6 +1,7 @@
 const { Product, validate } = require("../models/product.model");
 const { Cart } = require("../models/cart.model");
-
+const Mongoose = require('mongoose')
+const ObjectId = Mongoose.Types.ObjectId;
 class CartController {
     constructor() { }
     cartFind = async (req, res) => {
@@ -19,7 +20,7 @@ class CartController {
         try {
             const carts = await Cart.find({}).populate({
                 path: "items.productId",
-                select: "price total"
+                select: "title price total"
             });
             return carts[0];
 
@@ -28,11 +29,12 @@ class CartController {
         }
     }
     cart = async (req, res) => {
+        const {
+            productId
+        } = req.body;
+        const quantity = Number.parseInt(req.body.quantity);
         try {
-            console.log(req.body);
-
-            const productId = req.body.productId;
-            const quantity = req.body.quantity;
+            let cart = await this.cartAll();
             let productDetails = await Product.findById(productId);
             if (!productDetails) {
                 return res.status(500).json({
@@ -40,61 +42,51 @@ class CartController {
                     msg: "Invalid request"
                 })
             }
-
-            const carts = await this.cartAll();
-
-            if (carts) {
-
-                const indexFound = carts.items.findIndex(item => item.productId == productId);
-                /**
-                 *  this removes an item from the the cart if the quantity is set to zero,
-                 *  We can use this method to remove an item from the list 
-                 */
+            //--If Cart Exists ----
+            if (cart) {
+                //---- check if index exists ----
+                const indexFound = cart.items.findIndex(item => item.productId._id == productId);
+                //------this removes an item from the the cart if the quantity is set to zero,We can use this method to remove an item from the list  -------
                 if (indexFound !== -1 && quantity <= 0) {
-                    carts.items.splice(indexFound, 1);
-                    if (carts.items.length == 0) {
-                        carts.subTotal = 0;
+                    cart.items.splice(indexFound, 1);
+                    if (cart.items.length == 0) {
+                        cart.subTotal = 0;
                     } else {
-                        carts.subTotal = carts.items.map(item => item.total).reduce((acc, next) => acc + next);
+                        cart.subTotal = cart.items.map(item => item.total).reduce((acc, next) => acc + next);
                     }
                 }
-
-                /**
-                 * check if product exist,just add the previous quantity with the new quantity and update the total price
-                */
+                //----------check if product exist,just add the previous quantity with the new quantity and update the total price-------
                 else if (indexFound !== -1) {
-                    console.log(carts.items[indexFound].quantity)
-                    carts.items[indexFound].quantity = carts.items[indexFound].quantity + quantity;
-                    carts.items[indexFound].total = carts.items[indexFound].quantity * productDetails.price;
-                    carts.items[indexFound].price = productDetails.price
-                    carts.subTotal = carts.items.map(item => item.total).reduce((acc, next) => acc + next);
+                    cart.items[indexFound].quantity = cart.items[indexFound].quantity + quantity;
+                    cart.items[indexFound].total = cart.items[indexFound].quantity * productDetails.price;
+                    cart.items[indexFound].price = productDetails.price
+                    cart.subTotal = cart.items.map(item => item.total).reduce((acc, next) => acc + next);
                 }
-                //Check if Quantity is Greater than 0 then add item to items Array ----
+                //----Check if Quantity is Greater than 0 then add item to items Array ----
                 else if (quantity > 0) {
-                    carts.items.push({
+                    cart.items.push({
                         productId: productId,
                         quantity: quantity,
                         price: productDetails.price,
                         total: parseInt(productDetails.price * quantity)
                     })
-                    carts.subTotal = carts.items.map(item => item.total).reduce((acc, next) => acc + next);
+                    cart.subTotal = cart.items.map(item => item.total).reduce((acc, next) => acc + next);
                 }
-                // if quantity of price is 0 throw the error
-                // else {
-                //     return res.status(400).json({
-                //         type: "Invalid",
-                //         msg: "Invalid request"
-                //     })
-                // }
-                let data = await carts.save();
+                //----if quantity of price is 0 throw the error -------
+                else {
+                    return res.status(400).json({
+                        type: "Invalid",
+                        msg: "Invalid request"
+                    })
+                }
+                let data = await cart.save();
                 res.status(200).json({
                     type: "success",
                     mgs: "Process Successful",
                     data: data
-                });
+                })
             }
-            //if there is no user with a cart...it creates a new cart and then adds the item to the cart that has been created
-
+            //------------ if there is no user with a cart...it creates a new cart and then adds the item to the cart that has been created------------
             else {
                 const cartData = {
                     items: [{
@@ -105,14 +97,19 @@ class CartController {
                     }],
                     subTotal: parseInt(productDetails.price * quantity)
                 }
-                let cartItem = await Cart.create(cartData)
-                let data = await cartItem.save();
-                res.json(data);
+                cart = await Cart.create(cartData)
+                let data = await cart.save();
+                res.json(cart);
             }
-
-        } catch (e) {
-            res.json({ message: "Something went wrong " + e });
+        } catch (err) {
+            console.log(err)
+            res.status(400).json({
+                type: "Invalid",
+                msg: "Something Went Wrong",
+                err: err
+            })
         }
+
 
     };
 
