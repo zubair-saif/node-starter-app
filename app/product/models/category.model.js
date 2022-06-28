1d3b22eea5ed7d1650a14284e4f519fa8da5df06
zubair-saif <zubairsaif700@gmail.com>
2022-06-28 23:58:49 +0500

    category modal updated

--- app/product/models/category.model.js
+++ app/product/models/category.model.js
@@ -6,34 +6,13 @@ const schema = new mongoose.Schema(
     name: {
       type: String,
       trim: true,
+      required: true,
+      maxlength: 32,
+      unique: true,
     },
-    // image: {
-    //   contentType: String,
-    //   data: Buffer,
-    // },
-    description: {
-      type: String,
-      trim: true,
-    },
-    // isActive: {
-    //   type: Boolean,
-    //   default: true
-    // },
-    products: [
-      {
-        type: mongoose.Types.ObjectId,
-        ref: "Product",
-      },
-    ],
-    updated: Date,
-    created: {
-      type: Date,
-      default: Date.now,
-    },
-  }
-  // { versionKey: false, timestamps: true }
+  },
+  { timestamps: true }
 );
 
 const Category = mongoose.model("Category", schema);
-// module.exports.validate = validate;
 module.exports.Category = Category;
